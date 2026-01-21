10ff6f89bdc697b97d48bc161ff2a4f8cdc0c9d4
Varashree H A <varashree710@gmail.com>
2026-01-21 13:57:35 +0530

    Fixed Error

--- Backend/server.js
+++ Backend/server.js
@@ -9,37 +9,26 @@ import productRoutes from "./routes/product.route.js";
 dotenv.config();
 
 const app = express();
-
-// IMPORTANT: __dirname from project root
 const __dirname = path.resolve();
 
-/* DEV CORS */
-app.use(
-  cors({
-    origin: "http://localhost:5173",
-    methods: ["GET", "POST", "PUT", "DELETE"],
-    credentials: true,
-  })
-);
-
+/* Middleware */
 app.use(express.json());
+app.use(cors());
+
+/* API Routes */
 app.use("/api/products", productRoutes);
 
-/* PRODUCTION: serve React build */
+/* Serve frontend in production */
 if (process.env.NODE_ENV === "production") {
-  const frontendPath = path.join(__dirname, "Frontend", "dist");
-
-  app.use(express.static(frontendPath));
-
-  // REGEX catch-all (Node 24 safe)
+  app.use(express.static(path.join(__dirname, "Frontend/dist")));
   app.get(/.*/, (req, res) => {
-    res.sendFile(path.join(frontendPath, "index.html"));
+    res.sendFile(path.join(__dirname, "Frontend","dist","index.html"));
   });
 }
 
-/* START SERVER */
+/* Start server */
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   connectDB();
-  console.log(`Server running on http://localhost:${PORT}`);
+  console.log(`Server running on port ${PORT}`);
 });
